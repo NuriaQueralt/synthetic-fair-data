b67faaa43e7fd6788f9cbc9960b2ac1c3972764f
NuriaQueralt <nqueralt.r@gmail.com>
2020-11-25 12:24:23 +0100

    parsing IL-6 CLASS metadata from owl

--- scripts/rdf2srdf.py
+++ scripts/rdf2srdf.py
@@ -30,22 +30,52 @@ print("* The ontology has {} statements.".format(len(ont)))
 rdf_in.parse(who_ttl, format="ttl")
 print("* The who crf rdf has {} statements.".format(len(rdf_in)))
 
+# RDF DATA
 # parse rdf to csv
 # variable dictionary to store form:questions:
 # { crf1: { question: { value, unit, rule, range } }
 crf2question = dict()
 question2result = dict()
-results_list = list()
+result = dict()
 
 # parse
 import pprint
-for stmt in rdf_in:
-    pprint.pprint(stmt)
+#for stmt in rdf_in:
+    #pprint.pprint(stmt)
 
+# build a table for patient 1 :: IL-6 results
+
+# ONTOLOGY
 # parse ontology question structure
-# { question: onto_path }
+# { question: { str: onto_path } }
 question2ontoPath = dict()
 
+# find IL-6 subclass structure
+c = rdflib.URIRef("http://purl.org/vodan/whocovid19crfsemdatamodel/IL-6")
+pprint.pprint(ont.preferredLabel(c, lang="en"))
+pprint.pprint(ont.objects(subject=c, predicate=rdflib.RDFS.subClassOf))
+pprint.pprint(type(ont.objects(subject=c, predicate=rdflib.RDFS.subClassOf)))
+for parent in ont.objects(subject=c, predicate=rdflib.RDFS.subClassOf):
+    pprint.pprint(parent)
+
+# class
+question_type_list = list()
+for subclass in ont.objects(c, rdflib.RDFS.subClassOf):
+    pprint.pprint(subclass)
+
+for label in ont.objects(c, rdflib.RDFS.label):
+    pprint.pprint(label)
+
+for preflabel in ont.objects(c, rdflib.SKOS.prefLabel):
+    pprint.pprint(preflabel)
+
+# parent class
+
+
+# RULES/RANGES
+# { question: { rules: axioms, ranges: { dec, max, min }, units: unit_list } }
+# find axioms, e.g. subclassof
+# find ranges, both numeric and unit labels
 # variable array to store the tabular information:
 # ( crf_result x clinical parameter result )
 # e.g. crf1_module2_result3 x fever,il6,thrombosis
