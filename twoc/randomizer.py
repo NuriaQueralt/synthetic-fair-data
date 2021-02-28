6ca9b3e92987a748923ddd4c8e410d1c8d369ee9
NuriaQueralt <nqueralt.r@gmail.com>
2021-02-28 21:17:46 +0100

    finished v1.

--- twoc/randomizer.py
+++ twoc/randomizer.py
@@ -5,35 +5,54 @@
 # @author: Núria Queralt Rosinach
 # @email: n.queralt_rosinach@lumc.nl
 
-# TODO: code to generate synthetic fair data
 """Script to generate synthetic fair data from experimental design 1 ( random numbers of PGE2 on defined conditions ) for twoc"""
 
+from scipy.stats import norm, skewnorm
+import csv
+
+# input
 N = 5000
 S_noticu = N - N*0.02
 S_icu = N*0.02
 
-print("Number of patients: {}".format(N))
-print("Number of not severe patients (not icu): {}, severe(icu): {}".format(S_noticu, S_icu))
-print("Number of not severe patients on D: {}, severe not in D: {}".format(S_noticu*0.9, S_noticu + S_icu*0.9))
+# output
+out_file = 'out/exp1_random_sdata.csv'
+f = open(out_file, 'w')
+fieldNames = ['Patient_number', 'S', 'M', 'P']
+writer = csv.DictWriter(f, fieldnames=fieldNames)
+writer.writeheader()
+
+# algorithm
+#print("Number of patients: {}".format(N))
+#print("Number of not severe patients (not icu): {}, severe(icu): {}".format(S_noticu, S_icu))
+#print("Number of not severe patients on D: {}, severe not in D: {}".format(S_noticu*0.9, S_noticu + S_icu*0.9))
 
 i = 1
 while i <= N:
-#for i in range(N):
-    print(i)
+    # not severe patients (not ICU)
     if i <= S_noticu:
        S = 0
+       # on Dexamethasone (prob=0.9)
        if i <= S_noticu*0.9:
           M = 1
        else:
           M = 0
-       P = "gaussian value"
+       # PGE2 level: assign random value from normal distribution, [0.0, 1.0] mean=0.5
+       P = abs(norm.rvs(loc=0.5, scale=1))
+    # severe patients (ICU)
     else:   
        S = 1
+       # not on Dexamethasone (prob=0.9)
        if i <= S_noticu + S_icu*0.9:
           M = 0
        else:
           M = 1
-       P = "skewed value"
+       # PGE2 level: assign random value from a negative skew normal distribution, [0.0, 1.0] mean=0.95
+       P = abs(skewnorm.rvs(a=0.1, loc=0.95, scale=1))
     
     print("i: {}, S: {}, M: {}, P:{}".format(i, S, M, P))
+    writer.writerow({'Patient_number': i, 'S': S, 'M': M, 'P': round(P,4)})
     i += 1
+
+f.close()
+
